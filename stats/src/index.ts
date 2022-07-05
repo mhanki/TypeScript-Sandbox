8b0be064b1a5d4ba1b90a8f7d4c77ee26ce8a3fe
mhanki <meike.hankewicz@protonmail.com>
2022-07-05 13:03:41 +0200

    Add Summary, analyzer and reporter

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,8 +1,22 @@
-import { CsvFileReader } from './composition/CsvFileReader';
 import { MatchReader } from "./composition/MatchReader";
+import { Summary } from './Summary';
+/* import { CsvFileReader } from './composition/CsvFileReader';
+import { ConsoleReport } from './reporter/ConsoleReport';
+import { WinsAnalysis } from './analyzer/WinsAnalysis'; */
 
-const fileReader = new CsvFileReader('football.csv');
+/* const fileReader = new CsvFileReader('football.csv');
+
 const matchReader = new MatchReader(fileReader);
-
 matchReader.load();
-console.log(matchReader.matches[0])
+
+const winsSummary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+winsSummary.buildAndPrintReport(matchReader.matches) */
+
+const matchReader = MatchReader.fromCsv('football.csv');
+matchReader.load();
+
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+summary.buildAndPrintReport(matchReader.matches)
