66f546b08bfc2423e39e2e3a37c9eea89c95dced
mhanki <meike.hankewicz@protonmail.com>
2022-07-19 11:13:32 +0200

    Cleanup

--- projects/web-framework/src/views/UserEdit.ts
+++ projects/web-framework/src/views/UserEdit.ts
@@ -1,19 +1,14 @@
 import { View } from './View';
 import { User, UserProps } from '../models/User';
-import { UserForm } from '../views/UserForm';
-import { UserShow } from '../views/UserShow';
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
 
 export class UserEdit extends View<User, UserProps> {
-  regionsMap(): { [key: string]: string} {
+  regionsMap(): { [key: string]: string } {
     return {
       userShow: '.user-show',
       userForm: '.user-form'
     }
-  }
-
-  onRender(): void {
-    new UserShow(this.regions.userShow, this.model).render();
-    new UserForm(this.regions.userForm, this.model).render();
   }
 
   template(): string {
@@ -23,5 +18,10 @@ export class UserEdit extends View<User, UserProps> {
         <div class="user-form"></div>
       </div>
     `;
+  }
+
+  onRender(): void {
+    new UserShow(this.regions.userShow, this.model).render();
+    new UserForm(this.regions.userForm, this.model).render();
   }
 }
